4f5f09072b91aef5ec4d235ffa77bb201e335cdd
Eugene Rysaj <eugene.rysaj@gmail.com>
2020-03-06 02:29:46 +0200

    Make iOS example use SwiftUI

---
+++ iOS Example/Sources/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  iOS Example
+//
+//  Created by Eugene Rysaj on 06.03.2020.
+//  Copyright © 2020 Awesome Org. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
